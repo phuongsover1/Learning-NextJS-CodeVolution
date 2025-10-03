557cea2f83c329142f3bb698b1afe6a2dc00d66e
Phuong Nguyen <phuongsover2@gmail.com>
2025-10-03 16:31:44 +0700

    "feat: Add dynamic metadata for product detail pages
    Add generateMetadata function to dynamically set the page title based on the product ID, enhancing SEO and browser tab identification for individual product pages."

--- nextjs-codevolution/src/app/(after-login)/products/[productId]/page.tsx
+++ nextjs-codevolution/src/app/(after-login)/products/[productId]/page.tsx
@@ -1,6 +1,17 @@
-export default async function ProductDetail({ params }: {
+import { Metadata } from "next"
+
+type Props = {
     params: Promise<{ productId: string }>
-}) {
+}
+
+export const generateMetadata = async ({ params }: Props): Promise<Metadata> => {
+    const productId = (await params).productId;
+    return {
+        title: `Product ${productId}`
+    }
+}
+
+export default async function ProductDetail({ params }: Props) {
     const productId = (await params).productId
     console.log(typeof productId)
     return <h1>Product Detail {productId}</h1>
