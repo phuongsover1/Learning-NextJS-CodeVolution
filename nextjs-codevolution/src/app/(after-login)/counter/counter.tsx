28df698d3655870f695a4dcf53c50475118008e4
Phuong Nguyen <phuongsover2@gmail.com>
2025-10-03 16:55:09 +0700

    "feat: Add counter component with client-side state instead of making page.tsx as an client component, because client component cannot export metadata
    - Create new Counter component with useState hook
    - Add CounterPage wrapper with metadata
    - Implement increment functionality with button
    - Mark Counter component as client-side with 'use client' directive"

---
+++ nextjs-codevolution/src/app/(after-login)/counter/counter.tsx
@@ -0,0 +1,13 @@
+"use client"
+import { useState } from "react"
+
+export default function Counter() {
+
+  const [count, setCount] = useState(0)
+  return <>
+    <p>Count: {count}</p>
+    <div>
+      <button type="button" onClick={() => setCount(count + 1)}>Count up</button>
+    </div>
+  </>
+}
