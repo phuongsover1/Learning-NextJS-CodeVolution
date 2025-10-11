5405022ea8e688d9725e65a12bc0278f006909c4
Phương Nguyễn <phuongsover2@gmail.com>
2025-10-11 15:22:10 +0000

    feat: add articles section with list, layout, and dynamic article page
    - Create articles list page displaying available articles
    - Add ArticleLayout component for articles section
    - Implement dynamic article page with language selection and navigation

---
+++ nextjs-codevolution/src/app/(after-login)/articles/[articleId]/page.tsx
@@ -0,0 +1,49 @@
+import Link from "next/link";
+
+export default async function NewsArticle({
+  params,
+  searchParams,
+}: {
+  params: Promise<{ articleId: string }>;
+  searchParams: Promise<{ lang: "en" | "es" | "fr" }>;
+}) {
+  const { articleId } = await params;
+  const { lang = "en" } = await searchParams;
+  let displayLanguage;
+  switch (lang) {
+    case "fr":
+      displayLanguage = "France";
+      break;
+    case "es":
+      displayLanguage = "Espanol";
+      break;
+    case "en":
+    default:
+      displayLanguage = "English";
+      break;
+  }
+  return (
+    <div>
+      <h2 className="text-4xl">News article id : {articleId}</h2>
+      <p>Reading in language {displayLanguage}</p>
+      <h3 className="text-2xl">Reading in Language: </h3>
+      <ul className="list-disc">
+        <li>
+          <Link className="font-bold" href={`/articles/${articleId}?lang=en`}>
+            English
+          </Link>
+        </li>
+        <li>
+          <Link className="font-bold" href={`/articles/${articleId}?lang=fr`}>
+            France
+          </Link>
+        </li>
+      </ul>
+      <div>
+        <Link className="text-xl font-semibold" href="/articles">
+          Back to List of Article
+        </Link>
+      </div>
+    </div>
+  );
+}
