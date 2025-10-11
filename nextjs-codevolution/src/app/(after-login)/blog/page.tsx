8ef157a3efdcbace145d99fc78d0431588edd0c3
Phương Nguyễn <phuongsover2@gmail.com>
2025-10-11 16:50:52 +0000

    feat: add blog page with loading state
    - Create blog page with simulated async delay
    - Implement loading.tsx for blog loading state
    - Add loading component for better user experience

--- nextjs-codevolution/src/app/(after-login)/blog/page.tsx
+++ nextjs-codevolution/src/app/(after-login)/blog/page.tsx
@@ -1,4 +1,8 @@
-export default function Blog() {
-    return <h1>My Blog!</h1>
+export default async function Blog() {
+  await new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve("");
+    }, 3000);
+  });
+  return <h1>My Blog!</h1>;
 }
-
