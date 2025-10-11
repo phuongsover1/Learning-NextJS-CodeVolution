fb0bab716d0673b4567d6b5af016ff7987a95f5d
Phương Nguyễn <phuongsover2@gmail.com>
2025-10-11 14:12:02 +0000

    feat: add Tailwind CSS integration and implement authentication layout
    - Import Tailwind CSS in globals.css for global styling
    - Create AuthLayout with navigation links for login, register, and forgot password pages
    - Update main layout to use new global styles

--- nextjs-codevolution/src/app/(auth)/layout.tsx
+++ nextjs-codevolution/src/app/(auth)/layout.tsx
@@ -1,15 +1,40 @@
+"use client";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 
+const navLinks = [
+  {
+    name: "Login",
+    href: "/login",
+  },
+  { name: "Register", href: "/register" },
+  { name: "Forgot Password", href: "/forgot-password" },
+];
 export default function AuthLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
+  const pathname = usePathname();
   return (
     <>
-      <header style={{ backgroundColor: 'lightblue', padding: '1rem' }}>
+      <header style={{ backgroundColor: "lightblue", padding: "1rem" }}>
         <h1>Auth Page!!</h1>
+        {navLinks.map((link) => {
+          const isActive: boolean = pathname === link.href;
+          console.log(isActive);
+          return (
+            <Link
+              key={link.name}
+              className={isActive ? "font-bold mr-4" : "text-blue-500 mr-4"}
+              href={link.href}
+            >
+              {link.name}
+            </Link>
+          );
+        })}
       </header>
       {children}
     </>
-  )
+  );
 }
